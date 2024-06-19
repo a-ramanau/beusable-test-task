2f0715a4e8d666998bdadb50dd6242f88bea46e5
Anton Ramanau <antekromanov@gmail.com>
2024-06-19 21:30:20 +0200

    BU-1: Add openapi (swagger ui)

--- room-occupancy-manager/build.gradle.kts
+++ room-occupancy-manager/build.gradle.kts
@@ -6,6 +6,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation(libs.openapi)
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(libs.jsonunit)
